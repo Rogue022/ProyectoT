8e8d53dfb038a06ac6f8358762ae53c6a639046c
Leloo <bookrevenant35@gmail.com>
2025-02-05 12:15:09 -0600

    correcciones de seguridad

--- ParteAdmin/PruebaDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost', 'AdmnP', 'Revenant2159!', 'SistemaExamenes');
-if (!$conn) {
-    echo "Conexión fallida :(";
-}else{
-    echo "Conexión exitosa! <3";
-    exit;
-}
