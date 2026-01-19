973c7793176e86736f3b3b37c8384efc5abfb927
lilix <snoopy_invader182@proton.me>
2026-01-19 12:28:43 -0600

    cambios a la apariencia de indexA

--- Alumno/indexAlumno.php
+++ Alumno/indexAlumno.php
@@ -38,24 +38,24 @@
 
             <div class="main-cards">
                 <div class="card">
-                    <div class="card-inner">
+                    <div class="card-inner mx-auto">
                         <img src="ImgA/img1.png" style="width: 150px; height: 150px;" alt="Revisar examen">
                     </div>
-                    <h4>Revisión de examen</h4>
+                    <h4 class="card-title mx-auto">Revisión de examen</h4>
                 </div>
 
                 <div class="card">
-                    <div class="card-inner"><img src="ImgA/img2.png" style="width: 150px; height: 150px;" alt="Enviar información">
+                    <div class="card-inner mx-auto"><img src="ImgA/img2.png" style="width: 150px; height: 150px;" alt="Enviar información">
                     </div>
-                    <h4>Enviar información</h4>
+                    <h4 class="card-title mx-auto">Enviar información</h4>
 
                 </div>
 
                 <div class="card">
-                    <div class="card-inner">
+                    <div class="card-inner mx-auto">
                         <img src="ImgA/img3.png" style="width: 150px; height: 150px;" alt="Finalizar sesión">
                     </div>
-                    <h4>Finalizar sesión</h4>
+                    <h4 class="card-title mx-auto">Finalizar sesión</h4>
                 </div>
             </div>
         </main>
